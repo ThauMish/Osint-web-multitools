9ba4f14ac16f55f4e6f34f1315fa646ce5a20309
MjA0MjEwMWFjMWY2ZTc3NDFiZmU0M2YzNjcyZTZkN2M= <96048620+ThauMish@users.noreply.github.com>
2023-09-05 15:39:51 +0200

    Updated maigret

--- tasks.py
+++ tasks.py
@@ -5,11 +5,13 @@ import subprocess
 
 app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')
 SHERLOCK_PATH = '/root/project/sherlock/sherlock/sherlock.py'
+DAPROFILER_PATH = '/root/project/DaProfiler/profiler.py'
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 @app.task
 def run_sherlock_task(username):
-    cached_result = redis_client.get(username)
+    cache_key = f'holehe-{username}'
+    cached_result = redis_client.get(cache_key)
     if cached_result:
         return cached_result.decode('utf-8')
     
@@ -19,7 +21,8 @@ def run_sherlock_task(username):
 
 @app.task
 def run_holehe_task(email):
-    cached_result = redis_client.get(email)
+    cache_key = f'holehe-{email}'
+    cached_result = redis_client.get(cache_key)
     if cached_result:
         return cached_result.decode('utf-8')
     
@@ -27,9 +30,24 @@ def run_holehe_task(email):
     redis_client.set(email, result.stdout)
     return result.stdout
 
-
-
-
-
-
+@app.task
+def run_daprofiler_task(first_name, last_name):
+    cache_key = f'daprofiler-{first_name}-{last_name}'
+    cached_result = redis_client.get(cache_key)
+    if cached_result:
+        return cached_result.decode('utf-8')
     
+    result = subprocess.run(['python3', DAPROFILER_PATH, '-n', first_name, '-ln',  last_name], capture_output=True, text=True)
+    redis_client.set(cache_key, result.stdout)
+    return result.stdout
+
+@app.task
+def run_maigret_task(username):
+    cache_key = f'maigret-{username}'
+    cached_result = redis_client.get(cache_key)
+    if cached_result:
+        return cached_result.decode('utf-8')
+    
+    result = subprocess.run(['maigret', username, '--timeout 3'], capture_output=True, text=True)
+    redis_client.set(cache_key, result.stdout)
+    return result.stdout
